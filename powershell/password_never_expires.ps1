a813419dca41758ade64e4f886f24715a520392a
Jerry Linnihan <jr@ytmp.com>
2018-03-27 12:05:49 -0700

    Update password_never_expires.ps1

--- powershell/password_never_expires.ps1
+++ powershell/password_never_expires.ps1
@@ -1,4 +1,4 @@
 Import-Module ActiveDirectory
-Search-ADAccount  -SearchBase "OU=USA,DC=DrMartens,DC=local" â€“PasswordNeverExpires |
+Search-ADAccount  -SearchBase "OU=XXX,DC=XXX,DC=XXX" -PasswordNeverExpires |
 Select -Property Name,DistinguishedName |
 Export-CSV "C:\\Temp\PasswordNeverExpireADUsers.csv" -NoTypeInformation -Encoding UTF8
